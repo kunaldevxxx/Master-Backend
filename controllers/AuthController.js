c48b180fda8de1e168eb9c4bd31c4ffac6a23c53
Kunal Khare <136145336+kunaldevxxx@users.noreply.github.com>
2024-05-23 19:09:44 +0530

    Email Validation And SIgnup Completed

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 import prisma from "../DB/db.config.js";
 import vine from "@vinejs/vine";
 import { errors } from "@vinejs/vine";
+import bcrypt from "bcryptjs";
 import { messages } from "@vinejs/vine/defaults";
 import { registerSchema } from "../Validations/AuthValidation.js";
 class AuthController {
@@ -9,12 +10,32 @@ class AuthController {
             const body = req.body;
             const validator = vine.compile(registerSchema);
             const payload = await validator.validate(body);
-            return res.json({payload});
+            //check if email exist 
+            const findUser=  await prisma.users.findUnique({
+                where:{
+                    email:payload.email
+                }
+            })
+            if(findUser){
+                return res.status(400).json({errors:{
+                    email:"Email Already In Use",
+                }})
+            }
+            // Encrypt the Password 
+            const salt=bcrypt.genSaltSync(10)
+            payload.password=bcrypt.hashSync(payload.password,salt)
+            const user=await prisma.users.create({
+                data:payload
+            })
+            return res.json({status:200,message:"User Created Succesfully",user});
         }
         catch (error) {
             if (error instanceof errors.E_VALIDATION_ERROR) {
                 // console.log(error.messages)
                 return res.status(400).json({errors: error.messages})
+            }
+            else{
+                return res.status(500).json({status:500,message:"Something Went Wrong"})
             }
         }
     }
